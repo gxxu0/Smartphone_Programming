2a3c2f4709d7f2d76d19a2fe89d5773b2d8c1f88
gxxu0 <94434730+gxxu0@users.noreply.github.com>
2023-06-27 01:59:36 +0900

    Add files via upload

---
+++ build/generated/source/buildConfig/debug/kr/ac/baekseok/for_the_precious_woman/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package kr.ac.baekseok.for_the_precious_woman;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "kr.ac.baekseok.for_the_precious_woman";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
